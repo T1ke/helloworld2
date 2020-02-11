3e90add8c28f0bda4d3a5100d80c5bce668ccff2
Patrik Tirkkonen <patrik.tirkkonen@tuni.fi>
2020-02-11 12:29:46 +0200

    test commit once again

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,5 +3,11 @@
 int main()
 {
     std::cout << "Hello world!" << std::endl;
-  //  std::cout << "Terveppä terve, Maailma!" << std::endl;
+    std::cout << "Terveppä terve, Maailma!" << std::endl;
+
+    //this is a comment
+    for (int i = 0; i < 5; i++)
+    {//loops 
+        std::cout << i << std::endl;
+    }
 }
